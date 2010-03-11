9b2310897770bef8945d2787bf0ef70414bfe274
unknown <adm@.(none)>
2010-03-11 20:44:13 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Hello World</title>
+	<!--[if IE]>
+	  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<style>
+	  article, aside, figure, footer, header, hgroup,
+	  menu, nav, section { display: block; }
+	</style>
+
+</head>
+<body>
+	<div class="container">
+		<h1>Hello World do P H P ! </H1>
+		<p>Copyright &copy; <?php echo date ('H:i') ?> </p>
+	</div>
+</body>
+</html>
